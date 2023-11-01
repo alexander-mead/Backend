958056d305e705b9651f07b0c5ccf559f5ff0a58
Alexander Mead <alexander@digilab.co.uk>
2023-11-01 13:17:56 +0000

    formatting

--- static/logic.js
+++ static/logic.js
@@ -1,39 +1,37 @@
-
-const baseUrl = "http://localhost:8000"
+const baseUrl = "http://localhost:8000";
 
 function image() {
+  // Definitions
+  const xInput = document.getElementById("xInput").value;
+  const yInput = document.getElementById("yInput").value;
+  const zoomInput = document.getElementById("zoomInput").value;
+  const depthInput = document.getElementById("depthInput").value;
+  const url = baseUrl + "/image";
+  const params = {
+    method: "POST", // Unless this is present it will default to "GET"
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      real: xInput,
+      imag: yInput,
+      zoom: zoomInput,
+      depth: depthInput,
+    }),
+  };
 
-    // Definitions
-    const xInput = document.getElementById("xInput").value;
-    const yInput = document.getElementById("yInput").value;
-    const zoomInput = document.getElementById("zoomInput").value;
-    const depthInput = document.getElementById("depthInput").value;
-    const url = baseUrl+"/image";
-    const params = {
-        method: "POST", // Unless this is present it will default to "GET"
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            real: xInput,
-            imag: yInput,
-            zoom: zoomInput,
-            depth: depthInput,
-        })
-    };
-
-    // Fetch
-    fetch(url, params)
-        .then((response) => response.blob())
-        .then((blob) => {
-            console.log("Response blob received");
-            const objectURL = URL.createObjectURL(blob);
-            document.getElementById("image").src = objectURL; // To set image within html
-        })
-        //.then(document.getElementById("buttonId").disabled = true) // TODO: Disable button to prevent multiple requests
-        .then(console.log("Image displayed"))
-        .catch((error) => {
-            console.log('Error:', error)
-            console.log("Failed to sample image");
-        });
+  // Fetch
+  fetch(url, params)
+    .then((response) => response.blob())
+    .then((blob) => {
+      console.log("Response blob received");
+      const objectURL = URL.createObjectURL(blob);
+      document.getElementById("image").src = objectURL; // To set image within html
+    })
+    //.then(document.getElementById("buttonId").disabled = true) // TODO: Disable button to prevent multiple requests
+    .then(console.log("Image displayed"))
+    .catch((error) => {
+      console.log("Error:", error);
+      console.log("Failed to sample image");
+    });
 }
